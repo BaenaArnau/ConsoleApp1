8471c5aa21fbf1610a7083d016e7c702ceeb0964
Macnacka <93340013+BaenaArnau@users.noreply.github.com>
2024-09-04 11:01:30 +0200

    ejercicios n2,n3, acabados

--- ConsoleApp1/Consola.cs
+++ ConsoleApp1/Consola.cs
@@ -11,84 +11,124 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            while (true)
+            bool bucle = true;
+
+            while (bucle)
             {
                 int valorMenu;
                 
                 Menu();
                 valorMenu = int.Parse(Console.ReadLine());
 
-                if (valorMenu == 1) ConvertorDeTexto();
-                else if (valorMenu == 2) ComparadorFecha();
-                else if (valorMenu == 3) Polidromo();   
-                else if (valorMenu == 0) break;
-                else Console.WriteLine("Escriba una opcion valida");
+                switch (valorMenu)
+                {
+                    case 1:
+                        ConvertorDeUpAndDown();
+                        break;
+                    case 2:
+                        ComparadorFecha();
+                        break;
+                    case 3:
+                        Polidromo();
+                        break;
+                    case 4:
+                        Potencia();
+                        break;
+                    case 5:
+                        Dividir();
+                        break;
+                    case 6:
+                        ComparadorDeTexto();
+                        break;
+                    case 0:
+                        bucle = false;
+                        break;
+                    default:
+                        Console.WriteLine("Introduzca una opcion valida");
+                        break;
+                }
             }
         }
 
         public static void Menu()
         {
-            Console.WriteLine(@"1.    Comvertor de tecto a CamelCase o LowerCase
-2.    Comparador de fechas
-3.    Comprobar si una palabra es un polidromo
+            Console.WriteLine(@"
+1.      Comvertor de texto a CamelCase o LowerCase
+2.      Comparador de fechas
+3.      Comprobar si una palabra es un polidromo
+4.      Calcular potencia
+5.      Dividir
+6.      Comparar text alfabeticamente
 
 Presiona 0 para salir");
         }
 
-        public static void ConvertorDeTexto()
+        public static void ConvertorDeUpAndDown()
         {
             String cadena;
-            String nuevaCadena;
-            bool mayuscula;
-            bool minuscula;
-            string opciones;
+            String nuevaCadena = "";
+            bool mayuscula = true;
+            bool minuscula = true;
+            int opciones;
 
+            Console.WriteLine("Inserte la cadena de texto");
+            cadena = Console.ReadLine();
+            Console.WriteLine(@"
+1.      Pasar el texto a CamelCase
+2.      Pasar el texto a LowerCase");
+            opciones = int.Parse(Console.ReadLine());
 
-                nuevaCadena = "";
-                mayuscula = true;
-                minuscula = true;
+            switch (opciones)
+            {
+                case 1:
+                    Console.WriteLine(CamelAndLower(cadena, true));
+                    break;
+                case 2:
+                    Console.WriteLine(CamelAndLower(cadena, false));
+                    break;
+                default:
+                    Console.WriteLine("Introduzca una opcion valida");
+                    break;
+            }
+        }
 
-                Console.WriteLine("Inserte la cadena de texto");
-                cadena = Console.ReadLine();
-                Console.WriteLine("Quieres pasarlo a CamelCase o LowerCase");
-                opciones = Console.ReadLine();
-
-                if (opciones.ToLower().Equals("camelcase"))
+        public static string CamelAndLower(string cadena,bool camel)
+        {
+            string nuevaCadena = "";
+            bool letra = false;
+            if (camel)
+            {
+                foreach (char caracter in cadena)
                 {
 
-                    foreach (char caracter in cadena)
+                    if (caracter.Equals(' ')) 
+                        letra = true;
+                    else if (letra == true)
                     {
-
-                        if (caracter.Equals(' ')) mayuscula = true;
-                        else if (mayuscula == true)
-                        {
-                            nuevaCadena = nuevaCadena + Char.ToUpper(caracter);
-                            mayuscula = false;
-                        }
-                        else if (mayuscula == false) nuevaCadena = nuevaCadena + Char.ToLower(caracter);
-
+                        nuevaCadena = nuevaCadena + Char.ToUpper(caracter);
+                        letra = false;
                     }
-
-                    Console.WriteLine(nuevaCadena);
+                    else if (letra == false) 
+                        nuevaCadena = nuevaCadena + Char.ToLower(caracter);
                 }
-                else if (opciones.ToLower().Equals("lowercase"))
+
+            }
+            else
+            {
+                foreach (char caracter in cadena)
                 {
 
-                    foreach (char caracter in cadena)
+                    if (caracter.Equals(' ')) letra = true;
+                    else if (letra == true)
                     {
-
-                        if (caracter.Equals(' ')) minuscula = true;
-                        else if (minuscula == true)
-                        {
-                            nuevaCadena = nuevaCadena + Char.ToLower(caracter);
-                            minuscula = false;
-                        }
-                        else if (minuscula == false) nuevaCadena = nuevaCadena + Char.ToUpper(caracter);
+                        nuevaCadena = nuevaCadena + Char.ToLower(caracter);
+                        letra = false;
                     }
-
-                    Console.WriteLine(nuevaCadena);
+                    else if (letra == false) nuevaCadena = nuevaCadena + Char.ToUpper(caracter);
                 }
-                else Console.WriteLine("Introduzca una opcion valida");
+            }
+
+            return nuevaCadena;
         }
 
         public static void ComparadorFecha()
@@ -137,18 +177,110 @@ Presiona 0 para salir");
             Console.WriteLine("Escriba la palabra a continuacion:");
             entrada = Console.ReadLine().ToLower();
 
+            polidromo = ComparacionDeCaracteres(entrada);
+
+            if (polidromo) 
+                Console.WriteLine("La palabra " + entrada + " es un polinomio");
+            else 
+                Console.WriteLine("La palabra " + entrada + " no es un polinomio");
+        }
+
+        private static bool ComparacionDeCaracteres(string entrada)
+        {
             for (int i = 0; i < entrada.Length; i++)
             {
-                if (entrada[i] == entrada[entrada.Length-i-1]) polidromo = true;
-                else
-                {
-                    polidromo = false; 
-                    break;
-                }
+                if (entrada[i] != entrada[entrada.Length-i-1]) 
+                    return false; 
+            }
+            return true;
+        }
+
+        public static void Potencia()
+        {
+            int numero, potencia, resultado;
+
+            Console.WriteLine("Numero a multiplicar: ");
+            numero = int.Parse(Console.ReadLine());
+            resultado = numero;
+            Console.WriteLine("Numero de la potencia: ");
+            potencia = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < potencia; i++)
+            {
+                resultado = resultado * numero;
             }
 
-            if (polidromo) Console.WriteLine("La palabra " + entrada + " es un polinomio");
-            else Console.WriteLine("La palabra " + entrada + " no es un polinomio");
+            Console.WriteLine("Resultado: " + resultado);
+        }
+
+        public static void Dividir()
+        {
+            Console.WriteLine("Introduzca el numerador");
+            int numerador = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el denominador");
+            int denominador = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Resultado: " + ComparadorDivisores(numerador, denominador));
+        }
+
+        public static int ComparadorDivisores(int numerador, int denominador)
+        {
+            int contador = 1;
+
+            while (true)
+            {
+                if (numerador > denominador)
+                {
+                    numerador = numerador - denominador;
+                    contador++;
+                }
+                else
+                {
+                    return contador;
+                }
+            }
+        }
+
+        public static void ComparadorDeTexto()
+        {
+            Console.WriteLine("Escriba la primera palabra");
+            string palabra1 = Console.ReadLine();
+            Console.WriteLine("Escriba la segunda palabra");
+            string palabra2 = Console.ReadLine();
+
+            Console.WriteLine(ComparadorDePalabras(palabra1, palabra2));
+        }
+
+        public static bool ComparadorDePalabras(string palabra1, string palabra2)
+        {
+            if (palabra1.Length <= palabra2.Length)
+            {
+                for (int i = 0; i < palabra2.Length; i++)
+                {
+                    if (palabra1[i] != palabra2[i])
+                    {
+                        if (palabra1[i] > palabra2[i])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < palabra1.Length; i++)
+                {
+                    if (palabra1[i] != palabra2[i])
+                    {
+                        if (palabra1[i] > palabra2[i])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
         }
     }
 }
